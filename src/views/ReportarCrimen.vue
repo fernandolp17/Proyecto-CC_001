bddeee91ed14fd8152158ad214d9b5f295a90026
unknown <victor.ponce.p@uni.pe>
2020-08-28 13:21:48 -0500

    Tratando de centrar esa vaina

--- src/views/ReportarCrimen.vue
+++ src/views/ReportarCrimen.vue
@@ -11,30 +11,74 @@
       <v-subheader inset>Registro de Crimen</v-subheader>
       <v-subheader>Por favor, llene este formulario para tener más detalles del suceso:</v-subheader>
       <v-row justify="center">
-        <form>
-          <v-select
-            label="Tipo de crimen"
-            :items="items"
-            required
-          ></v-select>
-          <v-text-field
-            v-model="hora_del_suceso"
-            :error-messages="nameErrors"
-            label="Hora"
-            required
-            @input="$v.hora_del_suceso.$touch()"
-            @blur="$v.hora_del_suceso.$touch()"
-          ></v-text-field>
-          <v-textarea
-                  filled
-                  auto-grow
-                  label="Descripción adicional"
-                  rows="4"
-                  row-height="30"
-                  outlined
-                  v-model="textoAdicional"
-          ></v-textarea>
-        </form>
+        <v-col>
+          <v-row
+                  align="center"
+                  justify="center">
+            <form>
+              <v-select
+                      label="Tipo de crimen"
+                      :items="items"
+                      required
+              ></v-select>
+              <v-text-field
+                      v-model="hora_del_suceso"
+                      :error-messages="nameErrors"
+                      label="Hora"
+                      required
+                      @input="$v.hora_del_suceso.$touch()"
+                      @blur="$v.hora_del_suceso.$touch()"
+              ></v-text-field>
+              <v-textarea
+                      filled
+                      auto-grow
+                      label="Descripción adicional"
+                      rows="4"
+                      row-height="30"
+                      outlined
+                      v-model="textoAdicional"
+              ></v-textarea>
+            </form>
+            <v-dialog
+                    v-model="dialog"
+                    width="500"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                        color="red lighten-2"
+                        dark
+                        v-bind="attrs"
+                        v-on="on"
+                >
+                  Click Me
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title class="headline grey lighten-2">
+                  Privacy Policy
+                </v-card-title>
+
+                <v-card-text>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                          color="primary"
+                          text
+                          @click="dialog = false"
+                  >
+                    I accept
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-row>
+        </v-col>
       </v-row>
     </v-navigation-drawer>
     <v-app-bar dense app clipped-right clipped-left>
@@ -110,6 +154,7 @@ export default {
       drawerREGISTROCRIMEN: false,
       items: Object.keys(crimeTypes),
       textoAdicional : "",
+      dialog: false
     };
   },
   methods: {
