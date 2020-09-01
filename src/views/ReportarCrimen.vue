f0630931b0f5ff03730442f24f00e9077a765cb6
Espinoza Mansilla <37357283+Grol4Espinoza@users.noreply.github.com>
2020-09-01 10:38:06 -0500

    alineando btn, cambios de nombre,etc.

--- src/views/ReportarCrimen.vue
+++ src/views/ReportarCrimen.vue
@@ -1,85 +1,67 @@
 <template>
   <v-app>
     <v-navigation-drawer
-      v-model="drawerREGISTROCRIMEN"      
+      v-model="drawerREGISTROCRIMEN"
       app
       clipped
       temporary
       color="rgba(255, 212, 58, 0.85)"
       width="100%"
     >
-      <v-subheader inset>Registro de Crimen</v-subheader>
-      <v-subheader>Por favor, llene este formulario para tener más detalles del suceso:</v-subheader>
-      <v-row justify="center">
-        <v-col>
-          <v-row
-                  align="center"
-                  justify="center">
-            <form>
-              <v-select
-                      label="Tipo de crimen"
-                      :items="items"
-                      required
-              ></v-select>
-              <v-text-field
-                      v-model="hora_del_suceso"
-                      :error-messages="nameErrors"
-                      label="Hora"
-                      required
-                      @input="$v.hora_del_suceso.$touch()"
-                      @blur="$v.hora_del_suceso.$touch()"
-              ></v-text-field>
-              <v-textarea
-                      filled
-                      auto-grow
-                      label="Descripción adicional"
-                      rows="4"
-                      row-height="30"
-                      outlined
-                      v-model="textoAdicional"
-              ></v-textarea>
-            </form>
-            <v-dialog
-                    v-model="dialog"
-                    width="500"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                        color="red lighten-2"
-                        dark
-                        v-bind="attrs"
-                        v-on="on"
-                >
-                  Click Me
-                </v-btn>
-              </template>
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-card-title>Registro de Crimen</v-card-title>
+        </v-row>
+        <v-row align="center" justify="center">
+          <v-subheader
+            class="pa-10"
+          >Por favor, llene este formulario para tener más detalles del suceso:</v-subheader>
+        </v-row>
+        <v-row align="center" justify="center">
+          <form>
+            <v-select label="Tipo de crimen" :items="items" required></v-select>
+            <v-text-field
+              v-model="hora_del_suceso"
+              :error-messages="nameErrors"
+              label="Hora"
+              required
+              @input="$v.hora_del_suceso.$touch()"
+              @blur="$v.hora_del_suceso.$touch()"
+            ></v-text-field>
+            <v-textarea
+              filled
+              auto-grow
+              label="Descripción adicional"
+              rows="4"
+              row-height="30"
+              outlined
+              v-model="textoAdicional"
+            ></v-textarea>
+          </form>
+        </v-row>
+        <v-row align="center" justify="center">
+          <v-dialog v-model="dialog" width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">Finalizar Reporte</v-btn>
+            </template>
 
-              <v-card>
-                <v-card-title class="headline grey lighten-2">
-                  Privacy Policy
-                </v-card-title>
+            <v-card>
+              <v-card-title>Registro exitoso</v-card-title>
 
-                <v-card-text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </v-card-text>
+              <v-card-text>Gracias por su colaboración!</v-card-text>
 
-                <v-divider></v-divider>
+              <v-divider></v-divider>
 
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                          color="primary"
-                          text
-                          @click="dialog = false"
-                  >
-                    I accept
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-row>
-        </v-col>
-      </v-row>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <router-link to="/mapa">
+                  <v-btn color="primary" text @click="dialog = false">Ok</v-btn>
+                </router-link>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-row>
+      </v-container>
     </v-navigation-drawer>
     <v-app-bar dense app clipped-right clipped-left>
       <v-container fluid>
@@ -120,8 +102,7 @@
 </template>
 
 <script>
-
-const { crimeTypes } = require('../constantes/crime_enums');
+const { crimeTypes } = require("../constantes/crime_enums");
 
 import L from "leaflet";
 import { LMap, LTileLayer, LMarker, LPopup, LControl } from "vue2-leaflet";
@@ -153,8 +134,8 @@ export default {
       marker: null,
       drawerREGISTROCRIMEN: false,
       items: Object.keys(crimeTypes),
-      textoAdicional : "",
-      dialog: false
+      textoAdicional: "",
+      dialog: false,
     };
   },
   methods: {
